8dda29c1debcf09e29cc5f8c5da5fcbfbd37f78c
Stanislav Katkov <skatkov@cheddar.me>
2024-05-13 17:49:41 +0200

    gem boilerplate

---
+++ lib/munster.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "munster/version"
+
+module Munster
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/munster/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Munster
+  VERSION = "0.1.0"
+end
