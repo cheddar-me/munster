80e0ce6e798c17ec969dd4836c8d2f5200dfb58e
Stanislav Katkov <skatkov@cheddar.me>
2024-05-14 15:35:13 +0200

    don't force uuid in migration

--- example/db/migrate/20240514133445_create_munster_tables.rb
+++ example/db/migrate/20240514133445_create_munster_tables.rb
@@ -1,6 +1,7 @@
 class CreateMunsterTables < ActiveRecord::Migration[7.0]
+
   def change
-    create_table :munster_received_webhooks, id: :uuid do |t|
+    create_table :munster_received_webhooks, id: uuid do |t|
       t.string :handler_event_id, null: false
       t.string :handler_module_name, null: false
       t.string :status, default: "received", null: false
