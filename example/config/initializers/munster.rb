ede7d7e51c01364d40f5abf53b26814ef57de9a4
Stanislav Katkov <skatkov@cheddar.me>
2024-05-23 14:53:34 +0200

    remove table_name configuration

--- example/config/initializers/munster.rb
+++ example/config/initializers/munster.rb
@@ -4,5 +4,4 @@ require_relative "../../app/webhooks/test_handler"
 
 Munster.configure do |config|
   config.active_handlers = [WebhookTestHandler]
-  config.receive_webhooks_table_name = :munster_received_webhooks
 end
