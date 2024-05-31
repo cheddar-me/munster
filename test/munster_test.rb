749033986f8001e2af406c126537afa6f21d1006
Stanislav Katkov <skatkov@cheddar.me>
2024-05-31 15:33:35 +0200

    all passing tests

--- test/munster_test.rb
+++ test/munster_test.rb
@@ -6,8 +6,4 @@ class TestMunster < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Munster::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
