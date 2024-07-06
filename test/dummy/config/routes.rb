33aee2b44393a4721253d1fb5a464efe0cf9f936
Julik Tarkhanov <me@julik.nl>
2024-07-06 20:01:53 +0200

    Add a route to test path params

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  post "/per-user-munster/:user_id/:service_id", to: "munster/receive_webhooks#create"
   mount Munster::Engine => "/munster", :as => "webhooks"
 end
