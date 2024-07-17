2f02a9540bd69b3a4db537cb56db13d8fbcf8f3d
Julik Tarkhanov <me@julik.nl>
2024-07-17 14:36:06 +0100

    Continue

--- test/test_app.rb
+++ test/test_app.rb
@@ -2,6 +2,7 @@
 require 'active_record'
 require 'action_pack'
 require 'action_controller'
+require 'active_job'
 require 'rails'
 
 database = 'development.sqlite3'
@@ -34,7 +35,6 @@ class MunsterTestApp < Rails::Application
   config.secret_key_base = 'i_am_a_secret'
   config.active_support.cache_format_version = 7.0
   config.hosts << ->(host) { true } # Permit all hosts
-
   routes.append do
     mount Munster::Engine, at: "/munster"
     post "/per-user-munster/:user_id/private" => "munster/receive_webhooks#create"
