c4236d83255e8934662b7f4df961ff513e1cf962
Julik Tarkhanov <me@julik.nl>
2024-07-24 09:27:52 +0100

    Not needed anymore

--- example/app/webhooks/webhook_test_handler.rb
+++ example/app/webhooks/webhook_test_handler.rb
@@ -2,17 +2,11 @@
 
 # This handler accepts webhooks from our integration tests. This webhook gets dispatched
 # if a banking provider test fails, indicating that the bank might be having an incident
-
 class WebhookTestHandler < Munster::BaseHandler
   def valid?(request) = true
 
   def process(webhook)
-    return unless webhook.received?
-    webhook.update!(status: "processing")
-    webhook.update!(status: "processed")
-  rescue
-    webhook.update!(status: "error")
-    raise
+    Rails.logger.info { webhook.request.params.fetch(:payment_id) }
   end
 
   def expose_errors_to_sender? = true
