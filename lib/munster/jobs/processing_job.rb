213bf455f117e078a43bd9851122dff417e78107
Julik Tarkhanov <me@julik.nl>
2024-07-24 13:46:01 +0100

    Direct invalid webhook info to the error handler

--- lib/munster/jobs/processing_job.rb
+++ lib/munster/jobs/processing_job.rb
@@ -4,7 +4,12 @@ require "active_job" if defined?(Rails)
 
 module Munster
   class ProcessingJob < ActiveJob::Base
+    class WebhookPayloadInvalid < StandardError
+    end
+
     def perform(webhook)
+      Rails.error.set_context(munster_handler_module_name: webhook.handler_module_name, **Munster.configuration.error_context)
+
       webhook.with_lock do
         return unless webhook.received?
         webhook.processing!
@@ -14,7 +19,8 @@ module Munster
         webhook.handler.process(webhook)
         webhook.processed! if webhook.processing?
       else
-        Rails.logger.info { "#{webhook.class} #{webhook.id} did not pass validation and was skipped" }
+        e = WebhookPayloadInvalid.new("#{webhook.class} #{webhook.id} did not pass validation and was skipped")
+        Rails.error.report(e, handled: true, severity: :error)
         webhook.failed_validation!
       end
     rescue => e
